e2524cd96ff2206281ada201749bc9e74ec55e39
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-12 13:43:46 -0500

    Changed rectangle & text to placeholders

--- Medium View/ContentView.swift
+++ Medium View/ContentView.swift
@@ -15,23 +15,24 @@ struct ContentView: View {
             
             VStack (alignment: .trailing){
                 HStack (alignment: .bottom){
-                    Text("________")
-                        .font(.caption2)
+                    Text("Lorem ipsum")
+                      .font(.footnote)
                         .multilineTextAlignment(.trailing)
                         .frame(width: 20.0, height: 50.0)
-                    Rectangle()
+                    AsyncImage(url: URL(string: "https://dummyimage.com/640x360"))
                         .frame(width: 160.0, height: 150.0)
 
                 }
                 
                 HStack {
-                    Text("_____________________________________________________________________________________________________________________________________________________________________________________________________")
-                        .font(.footnote)
+                   Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus interdum posuere lorem ipsum. Vestibulum mattis ullamcorper velit sed ullamcorper. Morbi tincidunt ornare massa eget egestas purus viverra accumsan. Ante in nibh mauris cursus mattis molestie a. Orci ac auctor augue mauris. Adipiscing diam donec adipiscing tristique risus nec feugiat in fermentum. Sed elementum tempus egestas sed. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Nunc consequat interdum varius sit amet mattis. Egestas sed tempus urna et pharetra pharetra massa. Facilisis leo vel fringilla est ullamcorper. Justo laoreet sit amet cursus sit amet dictum sit amet. Consequat mauris nunc congue nisi vitae suscipit tellus. Sapien pellentesque habitant morbi tristique senectus et netus et. Cursus risus at ultrices mi tempus.")
+                      .font(.body)
+                     
                         .multilineTextAlignment(.leading)
                         .frame(width: 75.0, height:150.0)
                     
-                    Text("_______________________________________________________________________________________________________________________________________")
-                        .font(.footnote)
+                   Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed faucibus turpis in eu mi bibendum.")
+                      .font(.body)
                         .multilineTextAlignment(.leading)
                         .frame(width: 75.0, height: 150.0)
                 }
@@ -45,25 +46,28 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .frame(width: 160.0, height: 95.0)
                 HStack (alignment: .bottom){
-                    Text("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-                        .frame(width: 100.0)
-                    Rectangle()
+                   Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida cum sociis natoque penatibus. ")
+                      .font(.caption)
+                        .frame(width: 100.0, height: 50)
+                    AsyncImage(url: URL(string: "https://dummyimage.com/640x360"))
                         .frame(width: 40.0, height: 45.0)
-                    Text("_____")
+                   Text("Lorem ipsum")
+                      .font(.footnote)
                         .multilineTextAlignment(.leading)
                         .frame(width: 20.0, height: 35.0)
                 }
                 HStack (alignment: .top){
                     VStack{
-                        Text("_______________________________________________________________________________________________")
-
+                       Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida cum sociis natoque penatibus.")
+                          .font(.body)
                             .multilineTextAlignment(.leading)
                             .frame(width: 75.0, height: 75.0)
-                        Rectangle()
+                        AsyncImage(url: URL(string: "https://dummyimage.com/640x360"))
                             .frame(width: 75.0, height: 60.0)
                         
                     }
-                    Text("_______________________________________________________________________________________________________________________________________________________________________________________________________________________")
+                   Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida cum sociis natoque penatibus.")
+                      .font(.body)
                         .multilineTextAlignment(.leading)
                         .frame(width: 75.0, height: 150.0)
                 }
@@ -78,6 +82,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+       ContentView()
+          .preferredColorScheme(.light)
     }
 }
--- Medium View/Hard View.swift
+++ Medium View/Hard View.swift
@@ -9,19 +9,39 @@ import SwiftUI
 
 struct Hard_View: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("DATA VISUALIZATION")
                 .font(.largeTitle)
-            Rectangle()
-                .padding(.top, 20.0)
 
-            HStack{
-                Text("__________________________________________________________________________")
-                Text("__________________________________________________________________________")
-                Text("__________________________________________________________________________")
+            AsyncImage(url: URL(string: "https://dummyimage.com/640x360"))
+                .frame(width: 600.0, height: 230.0)
+                       
+
+            HStack(alignment: .top){
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed faucibus turpis in eu mi bibendum.    ")
+                    .font(.body)
+                    .padding(.trailing, 10.0)
+                    .frame(width: 110.0, height: 200.0)
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida cum sociis natoque penatibus.  ")
+                    .font(.body)
+                    .multilineTextAlignment(.leading)
+                    .frame(width: 100.0, height: 200.0)
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque elit eget gravida cum.  ")
+                    .font(.body)
+                    .frame(width: 100.0, height: 200.0)
                 ZStack{
-                    Text("__________________________________________________________________________")
-                    Text("__________________________________________________________________________")
+                    HStack {
+                        Text("Egestas pretium aenean pharetra magna ac. Arcu odio ut sem nulla. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.")
+                            .frame(width: 100.0, height: 200.0)
+                        Text("Gravida rutrum quisque non tellus orci ac auctor augue mauris. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. ")
+                            .frame(width: 100.0, height: 200.0)
+                    }
+                    Circle()
+                        .frame(width: 100.0, height: 100.0)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    Text("Pull at Quote")
+                        .font(.title3)
+                        .frame(width: 50.0)
                 }
             }
         }
@@ -32,5 +52,6 @@ struct Hard_View: View {
 struct Hard_View_Previews: PreviewProvider {
     static var previews: some View {
         Hard_View()
+            .preferredColorScheme(.light)
     }
 }
