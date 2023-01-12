291a3494a4caf35ad3dc11089b280d44859dd2bb
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-11 21:44:44 -0500

    Finished the second view

--- Medium View/ContentView.swift
+++ Medium View/ContentView.swift
@@ -9,11 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack (alignment: .top){
+            
+            
+            
+            VStack (alignment: .trailing){
+                HStack (alignment: .bottom){
+                    Text("________")
+                        .font(.caption2)
+                        .multilineTextAlignment(.trailing)
+                        .frame(width: 20.0, height: 50.0)
+                    Rectangle()
+                        .frame(width: 160.0, height: 150.0)
+
+                }
+                
+                HStack {
+                    Text("_____________________________________________________________________________________________________________________________________________________________________________________________________")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 75.0, height:150.0)
+                    
+                    Text("_______________________________________________________________________________________________________________________________________")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 75.0, height: 150.0)
+                }
+            }
+            
+            
+            
+            
+            VStack (alignment: .leading){
+                Text("BIG TITLE GOES HERE AND HERE")
+                    .font(.largeTitle)
+                    .frame(width: 160.0, height: 95.0)
+                HStack (alignment: .bottom){
+                    Text("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                        .frame(width: 100.0)
+                    Rectangle()
+                        .frame(width: 40.0, height: 45.0)
+                    Text("_____")
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 20.0, height: 35.0)
+                }
+                HStack (alignment: .top){
+                    VStack{
+                        Text("_______________________________________________________________________________________________")
+
+                            .multilineTextAlignment(.leading)
+                            .frame(width: 75.0, height: 75.0)
+                        Rectangle()
+                            .frame(width: 75.0, height: 60.0)
+                        
+                    }
+                    Text("_______________________________________________________________________________________________________________________________________________________________________________________________________________________")
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 75.0, height: 150.0)
+                }
+                
+            }
+           
+            
         }
         .padding()
     }
