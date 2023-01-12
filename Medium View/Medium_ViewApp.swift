6f047069e968b216b2e4d5e987d8b5a56e4619e7
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-11 21:24:49 -0500

    Initial Commit

---
+++ Medium View/Medium_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medium_ViewApp.swift
+//  Medium View
+//
+//  Created by Evelyn Chen on 2023-01-11.
+//
+
+import SwiftUI
+
+@main
+struct Medium_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
